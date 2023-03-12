c824f23c8cb8faa725635679bd7490955c402b1e
Zhenbang You <1800012963@pku.edu.cn>
2023-03-11 20:49:02 -0800

    Add initialized declaration and for loop

--- src/Statement.scala
+++ src/Statement.scala
@@ -12,6 +12,10 @@ case class Declaration[T <: Type](variable: T) extends Statement {
     def codeGen: String = variable.defName
 }
 
+case class InitializedDeclaration[T <: Type](variable: T, initVal: PolyExpr[T]) extends Statement {
+    def codeGen: String = s"${variable.defName.split('=').head}= ${initVal.codeGen};\n"
+}
+
 case class IfStmt(cond: BoolExpr)(thenBody: Statement)(elseBody: Statement) extends Statement {
     def codeGen: String = s"if(${cond.codeGen}) {\n\t${thenBody.codeGen}} else {\n\t${elseBody.codeGen}}\n"
 
@@ -19,4 +23,8 @@ case class IfStmt(cond: BoolExpr)(thenBody: Statement)(elseBody: Statement) exte
 
 case class WhileStmt(cond: BoolExpr)(body: Statement) extends Statement {
     def codeGen: String = s"while (${cond.codeGen}) {\n\t${body.codeGen}}\n"
+}
+
+case class For(init: Statement, cond: BoolExpr, post: Statement)(body: Statement) extends Statement {
+    override def codeGen: String = s"for (${init.codeGen}; ${cond.codeGen}; ${post.codeGen}) {\n\t${body.codeGen}}\n"
 }
