9ef41c34fd5259b0e15c9b564f9ad0206a4217c7
Zhenbang You <1800012963@pku.edu.cn>
2023-03-12 12:49:33 -0700

    ver0.10.3: introduce multi-line string of scala, fix some bugs

--- src/Func.scala
+++ src/Func.scala
@@ -7,12 +7,16 @@ sealed trait Func {
 case class GlobalFunc[T <: Type](name: String)(args: Type*)(body: PolyExpr[T]) extends Func {
     def codeGen: String = {
         val resultRefType = body.refTypeName // return type of the function, in reference type
-        val RESULT_ARG = "result"
-        val RESULT_ADDR = if (!body.isInstanceOf[TmpFloatArrayAccess]) "*result" else "result[idx]"
+        val resultArg = "result"
+        val resultAddress = if (!body.isInstanceOf[TmpFloatArrayAccess]) "*result" else "result[idx]"
         val namesInArgs = args.map(_.argsName.head)
         val sizeInArgs = args.filter(_.argsName.length == 2).map(_.argsName(1)).toSet.toList
-        val argList = (namesInArgs ++ sizeInArgs :+ s"$resultRefType $RESULT_ARG").reduce((a, b) => s"$a, $b")
-        s"__global__ void $name($argList) {\n\t${Index.defineIdx}\n${statements2String(body.genStatements, "\t")}\t*result = ${body.getResult.varName};\n}\n"
+        val argList = (namesInArgs ++ sizeInArgs :+ s"$resultRefType $resultArg").reduce((a, b) => s"$a, $b")
+        s"""__global__ void $name($argList) {
+           |\t${Index.defineIdx}
+           |${statements2String(body.genStatements, "\t").stripTrailing}
+           |\t${resultAddress} = ${body.getResult.varName};
+           |}""".stripMargin
     }
 }
 
@@ -22,7 +26,10 @@ case class DeviceFunc[T <: Type](name: String)(args: Type*)(body: PolyExpr[T]) e
         val namesInArgs = args.map(_.argsName.head)
         val sizeInArgs = args.filter(_.argsName.length == 2).map(_.argsName(1)).toSet.toList
         val argList = if ((namesInArgs ++ sizeInArgs).isEmpty) "" else (namesInArgs ++ sizeInArgs).reduce((a, b) => s"$a, $b")
-        val bodyCode = s"return ${body.codeGen};\n"
-        s"__device__ $resultType $name($argList) {\n\t${Index.defineIdx}\n${statements2String(body.genStatements, "\t")}\treturn ${body.getResult.varName};\n}\n"
+        s"""__device__ $resultType $name($argList) {
+           |\t${Index.defineIdx}
+           |${statements2String(body.genStatements, "\t").stripTrailing}
+           |\treturn ${body.getResult.varName};
+           |}""".stripMargin
     }
 }
--- src/Statement.scala
+++ src/Statement.scala
@@ -13,26 +13,31 @@ case class Declaration[T <: Type](variable: T) extends Statement {
 }
 
 case class InitializedDeclaration[T <: Type](variable: T, initVal: PolyExpr[T]) extends Statement {
-    def codeGen: String = s"${variable.defName.split('=').head}= ${initVal.codeGen};\n"
+    def codeGen: String = s"${variable.defName.split('=').head} = ${initVal.codeGen};\n"
 }
 
 case class IfStmt(cond: BoolExpr)(thenBody: Vector[Statement])(elseBody: Vector[Statement]) extends Statement {
-    def codeGen: String = s"if ${cond.codeGen} {\n${
-        statements2String(thenBody, "\t")
-    }} else {\n${statements2String(thenBody, " \t")}}\n"
+    def codeGen: String =
+        s"""if ${cond.codeGen} {
+           |${statements2String(thenBody, "\t").stripTrailing}
+           |} else {
+           |${statements2String(elseBody, " \t").stripTrailing}
+           |}""".stripMargin
 
 }
 
 case class WhileLoop(cond: BoolExpr)(body: Vector[Statement]) extends Statement {
-    def codeGen: String = s"while ${cond.codeGen} {\n\t${
-        statements2String(body, "\t")
-    }}\n"
+    def codeGen: String =
+        s"""while ${cond.codeGen} {
+           |${statements2String(body, "\t").stripTrailing}
+           |}""".stripMargin
 }
 
 case class For(init: Statement, cond: BoolExpr, post: Statement)(body: Vector[Statement]) extends Statement {
-    override def codeGen: String = s"for (${init.codeGen}; ${cond.codeGen}; ${post.codeGen}) {\n\t${
-        statements2String(body, "\t")
-    }}\n"
+    override def codeGen: String =
+        s"""for (${init.codeGen}; ${cond.codeGen}; ${post.codeGen}) {
+           |${statements2String(body, "\t")}
+           |}""".stripMargin
 }
 
 def statements2String(statements: Vector[Statement], prepend: String): String =
